79fcc8362b6084b2ec8ab337dfc5c493e6b3477c
nanan0704 <86632289+nanan0704@users.noreply.github.com>
2022-06-04 12:13:15 +0800

    Initial commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,6 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://tokens.bscswap.com/tokens.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+]
